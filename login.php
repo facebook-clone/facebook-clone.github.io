f38b2112f82fa15675175c56872adc65bb7efa2e
RaymondJiang1 <raymondjiang@casady.org>
2020-09-22 18:37:33 -0500

    Started making login\signup page

---
+++ login.php
@@ -0,0 +1,28 @@
+<html>
+
+	<head>
+
+    	<title>Facebook</title>
+	</head>
+
+	<style>
+		
+		#bar{
+			height: 100px;
+			background-color: rgb(59,89,152);
+			color: #d9dfeb;
+			font-size: 40px;
+		}
+
+	</style>
+
+	<body>
+
+    	<div id="bar">
+    	Facebook Signup 
+    	</div>
+
+	</body>
+
+
+</html>
