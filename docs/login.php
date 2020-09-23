f9e1ebf3125bf8f990e3a84a5ce1cbb2ab8fbc13
RaymondJiang1 <raymondjiang@casady.org>
2020-09-23 15:53:35 -0500

    Added signup page :)

--- docs/login.php
+++ docs/login.php
@@ -1,101 +1,80 @@
 <html>
 
-	<head>
+<head>
 
-    	<title>Facebook</title>
-	</head>
+	<title>Facebook</title>
+</head>
 
-	<style>
-		
-		#bar {
-			height: 100px;
-			background-color: rgb(59,89,152);
-			color: #d9dfeb;
-			font-size: 40px;
-			padding: 4px;
-		}
+<style>
+	#bar {
+		height: 100px;
+		background-color: rgb(59, 89, 152);
+		color: #d9dfeb;
+		font-size: 40px;
+		padding: 4px;
+	}
 
-		#signup_button {
-			background-color: #42b72a;
-			width: 150px;
-			text-align: center;
-			padding: 4px;
-			border-radius: 4px;
-			float: right;
-		}
+	#signup_button {
+		background-color: #42b72a;
+		width: 150px;
+		text-align: center;
+		padding: 4px;
+		border-radius: 4px;
+		float: right;
+	}
 
-		#bar-2 {
-			background-color: white; 
-			width: 800px;
-			height: 200px; 
-			margin: auto; 
-			margin-top: 50px;
-			padding: 10px;
-			padding-top: 50px;
-			text-align: center;
-		}
+	#bar-2 {
+		background-color: white;
+		width: 800px;
+		height: 200px;
+		margin: auto;
+		margin-top: 50px;
+		padding: 10px;
+		padding-top: 50px;
+		text-align: center;
+	}
 
-		#text {
-			height: 40px;
-			width: 300px;
-			border-radius: 4px;
-			border: solid 1px #ccc;
-			padding: 4px;
-			font-size: 14px;
-			font-weight: bold;
-		}
+	#text {
+		height: 40px;
+		width: 300px;
+		border-radius: 4px;
+		border: solid 1px #ccc;
+		padding: 4px;
+		font-size: 14px;
+		font-weight: bold;
+	}
 
-		#button {
-			width: 300px;
-			height: 40px;
-			border-radius: 4px;
-			font-weight: bold;
-			border: none;
-			background-color: rgb(59, 89, 152);
-			color: white;
-		}
+	#button {
+		width: 300px;
+		height: 40px;
+		border-radius: 4px;
+		font-weight: bold;
+		border: none;
+		background-color: rgb(59, 89, 152);
+		color: white;
+	}
+</style>
 
-	</style>
+<body style="font-family: tahoma; background-color: #e9ebee;">
 
-	<body style="font-family: tahoma; background-color: #e9ebee;">
+	<div id="bar">
 
-    	<div id="bar">
+		<div style="font-size: 40px;">Facebook</div>
+		<div id="signup_button">Signup</div>
 
-    		<div style="font-size: 40px;">Facebook</div>
-    		<div id="signup_button">Signup</div>
+	</div>
 
-    	</div>
+	<div id="bar-2">
+		Log in to Facebook<br><br>
 
-    	<div id="bar-2">
-    		Log in to Facebook<br><br>
+		<input type="text" id="text" placeholder="Email"><br><br>
+		<input type="password" id="text" placeholder="Password"><br><br>
 
-    		<input type="text" id="text" placeholder="Email"><br><br>
-    		<input type="password" id="text" placeholder="Password"><br><br>
+		<input type="submit" id="button" value="Login">
+		<br><br><br>
+	</div>
 
-    		<input type="submit" id="button" value="Login">
-    		<br><br><br>
-    	</div>
-
-	</body>
+</body>
 
 
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ docs/signup.php
@@ -0,0 +1,91 @@
+<html>
+
+<head>
+
+    <title>Facebook | Sign up</title>
+</head>
+
+<style>
+    #bar {
+        height: 100px;
+        background-color: rgb(59, 89, 152);
+        color: #d9dfeb;
+        font-size: 40px;
+        padding: 4px;
+    }
+
+    #signup_button {
+        background-color: #42b72a;
+        width: 150px;
+        text-align: center;
+        padding: 4px;
+        border-radius: 4px;
+        float: right;
+    }
+
+    #bar-2 {
+        background-color: white;
+        width: 800px;
+        height: auto;
+        margin: auto;
+        margin-top: 50px;
+        padding: 10px;
+        padding-top: 50px;
+        text-align: center;
+    }
+
+    #text {
+        height: 40px;
+        width: 300px;
+        border-radius: 4px;
+        border: solid 1px #ccc;
+        padding: 4px;
+        font-size: 14px;
+        font-weight: bold;
+    }
+
+    #button {
+        width: 300px;
+        height: 40px;
+        border-radius: 4px;
+        font-weight: bold;
+        border: none;
+        background-color: rgb(59, 89, 152);
+        color: white;
+    }
+</style>
+
+<body style="font-family: tahoma; background-color: #e9ebee;">
+
+    <div id="bar">
+
+        <div style="font-size: 40px;">Facebook</div>
+        <div id="signup_button">Login</div>
+
+    </div>
+
+    <div id="bar-2">
+        Sign up to Facebook<br><br>
+
+        <input type="text" id="text" placeholder="First Name"><br><br>
+        <input type="text" id="text" placeholder="Last Name"><br><br>
+
+        <span style="font-weight: normal;">Gender:</span><br>
+        <select id="text">
+            <option>Male</option>
+            <option>Female</option>
+        </select>
+        <br><br>
+        <input type="text" id="text" placeholder="Email"><br><br>
+
+        <input type="password" id="text" placeholder="Password"><br><br>
+        <input type="password" id="text" placeholder="Retype Password"><br><br>
+
+        <input type="submit" id="button" value="Sign up">
+        <br><br><br>
+    </div>
+
+</body>
+
+
+</html>
